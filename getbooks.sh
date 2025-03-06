5d0b610175f993469d5e51985040e1d9582ebb49
bscruz <143441734+bscruz@users.noreply.github.com>
2025-03-05 22:27:12 -0800

    added cool echos

--- getbooks.sh
+++ getbooks.sh
@@ -9,9 +9,9 @@ do
     x=$(curl -s  https://www.gutenberg.org/dirs/GUTINDEX.ALL | grep -w -o -i -E -A 1 "^${book}[,] by ${author}+[[:blank:]]+[[:digit:]]{3,5}" |  grep -E -B 1 '^\s*$' | grep -E -o "[[:digit:]]{3,5}" ) # get the book number from website    
     author_dir=$(echo $author | tr A-Z a-z | sed 's/[[:space:]]/_/g') # changes format of author input lowercase
     book_file=$(echo $book | tr A-Z a-z | sed 's/[[:space:]]/_/g')
-    check_author="$(find "$authordir" -maxdepth 1 -type d| sed 's/.\///g')" # looks for a directory named after author
+    
                                                                                                                       
-    if [ -z $check_author ];
+    if [ ! -d "$author_dir" ];
     then
 	mkdir "$author_dir"
         echo "Created directory: $author_dir"
@@ -20,7 +20,7 @@ do
     fi 
 
     wget -q https://www.gutenberg.org/cache/epub/$x/pg$x.txt -O $author_dir/$book_file.txt # downloads the book  
-
+    echo "$book has been DOWNLOADED"
 done < "$file"
 
 
