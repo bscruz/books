fb681ea40b63ab705c7acbb15c05586c2acb2dc9
bscruz <143441734+bscruz@users.noreply.github.com>
2025-03-05 23:17:19 -0800

    actually followed the prompt this time

---
+++ followprompt.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+file=$1
+
+while IFS= read -r line # starts a loop that reads each line because IFS is empty reads the unmod line very important
+do
+    book=$(curl -s $line | grep "Title:"| sed 's/Title: //g') #gets the name of the book from the file
+    echo $book
+    author=$(curl -s $line | grep "Author:"| sed 's/Author: //g') # gets the author of the book from the file          
+    author_dir=$(echo $author | tr A-Z a-z | sed 's/[[:space:]]/_/g' | sed 's/_$//') # changes format of author input lowercase
+    book_file=$(echo $book | tr A-Z a-z | sed 's/[[:space:]]/_/g' | sed 's/_$//')
+    
+                                                                                                                      
+    if [ ! -d "$author_dir" ];
+    then
+	mkdir "$author_dir"
+        echo "Created directory: $author_dir"
+    else
+        echo "Directory $author_dir already exists"
+    fi 
+
+    wget -q $line -O $author_dir/$book_file.txt # downloads the book  
+
+
+    echo "DOWNLOAD COMPLETE for $book"
+done < "$file"
+
+
+
+    
